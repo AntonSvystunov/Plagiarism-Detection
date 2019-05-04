8e70987ae10d5a33b95cd259abd163f36962a66c
Anton <antonsvistunov99@gmail.com>
2019-05-04 15:10:48 +0300

    LevenshteinDistanceLexeme added

--- Plagiarism/Chunk.cs
+++ Plagiarism/Chunk.cs
@@ -137,6 +137,68 @@ namespace Plagiarism
             return distanceMatrix[n, m];
         }
 
+        public static double LevenshteinDistanceLexeme(string[] str1, string[] str2)
+        {
+            int n = str1.Length;
+            int m = str2.Length;
+            double[,] distanceMatrix = new double[n + 1, m + 1];
+
+            if (n == 0) return m;
+            if (m == 0) return n;
+
+            for (int i = 0; i <= n; distanceMatrix[i, 0] = i++) ;
+            for (int i = 0; i <= m; distanceMatrix[0, i] = i++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    double distance = FindLongestCommonLexemeRowCoef(str1[i - 1], str2[j - 1]);
+
+                    distanceMatrix[i, j] = Math.Min(
+                        (double)Math.Min(distanceMatrix[i - 1, j] + 1, distanceMatrix[i, j - 1] + 1),
+                        distanceMatrix[i - 1, j - 1] + distance);
+                }
+            }
+
+            return distanceMatrix[n, m];
+        }
+
+        public static double FindLongestCommonLexemeRowCoef(string str1, string str2)
+        {
+            var lexer = new LexicalAnalyzer();
+
+            List<Lexeme> lex1 = lexer.process(str1);
+
+            lexer = new LexicalAnalyzer();
+            List<Lexeme> lex2 = lexer.process(str2);
+
+            lex1.RemoveAll(lex => lex.type == LexerType.COMMENT);
+            lex2.RemoveAll(lex => lex.type == LexerType.COMMENT);
+
+            int[,] a = new int[lex1.Count + 1, lex2.Count + 1];
+            int row = 0;
+            int col = 0;
+
+            for (var i = 0; i < lex1.Count; i++)
+            {
+                for (var j = 0; j < lex2.Count; j++)
+                {
+                    if (lex1[i].type == lex2[j].type)
+                    {
+                        int len = a[i + 1, j + 1] = a[i, j] + 1;
+                        if (len > a[row, col])
+                        {
+                            row = i + 1;
+                            col = j + 1;
+                        }
+                    }
+                }
+            }
+
+            return lex1.GetRange(row - a[row, col], a[row, col]).Count / lex1.Count;
+        }
+
         public static List<Lexeme> FindLongestCommonLexemeRow(string str1, string str2)
         {
             var lexer = new LexicalAnalyzer();
