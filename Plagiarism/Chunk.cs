65efafc2a9bd1f8702ccc155b3ad668e737c06f8
bogdanloza <lozabogdan00@gmail.com>
2019-05-04 15:00:34 +0300

    Added for array leveshtein

--- Plagiarism/Chunk.cs
+++ Plagiarism/Chunk.cs
@@ -110,6 +110,33 @@ namespace Plagiarism
             return distanceMatrix[n, m];
         }
 
+        public static int LevenshteinDistance(string[] str1, string[] str2)
+        {
+            int n = str1.Length;
+            int m = str2.Length;
+            int[,] distanceMatrix = new int[n + 1, m + 1];
+
+            if (n == 0) return m;
+            if (m == 0) return n;
+
+            for (int i = 0; i <= n; distanceMatrix[i, 0] = i++) ;
+            for (int i = 0; i <= m; distanceMatrix[0, i] = i++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int distance = (str1[i - 1] == str2[j - 1]) ? 0 : 1;
+
+                    distanceMatrix[i, j] = Math.Min(
+                        Math.Min(distanceMatrix[i - 1, j] + 1, distanceMatrix[i, j - 1] + 1),
+                        distanceMatrix[i - 1, j - 1] + distance);
+                }
+            }
+
+            return distanceMatrix[n, m];
+        }
+
         public static List<Lexeme> FindLongestCommonLexemeRow(string str1, string str2)
         {
             var lexer = new LexicalAnalyzer();
